32c1329872823ac916bd2f82ff9b69907fb01e5f
ratul <ratulshourov01@gmail.com>
2023-08-03 23:49:53 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+import { useState } from 'react';
+import Nav from './components/Nav';
 function App() {
+  const [text, setText] = useState('hello');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Nav title="NavBar2"></Nav>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>I am from Header Component</div>
+  )
+}
+
+export default Header
